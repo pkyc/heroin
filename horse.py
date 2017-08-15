7ba711a0291db872ad72843331821c7cef7c208e
root <peterkycheung@gmail.com>
2017-08-15 09:41:22 +0000

    almost paree the tddata

--- horse.py
+++ horse.py
@@ -17,15 +17,14 @@ def readhor():
     for a in cursor:
         link = a["link"].encode("utf-8")
         name = a["name"].encode("utf-8")
-        if counter > 1:
+        if counter > 3:
             break
         gethor(name,link)
         counter += 1
     return
 
 def gethor(n,l):
-    file = open('output.out', 'w')
-    print(n,l)
+    file = open('output.json', 'w')
     hjson = "{'NAME' : '" + n + "', 'LINK': '" + l + "'}"
     htmlfile = urllib2.urlopen(l)
     htmltext = htmlfile.read()
@@ -38,11 +37,21 @@ def gethor(n,l):
     # horse in list []
     horsedict = []
     for h in header:
-        horsedict.append(h.text.strip().encode("utf-8"))
+        h = h.text.strip().encode("utf-8")
+        if h == "VideoReplay":
+            pass
+        else:
+            horsedict.append(h)
 
+    #testing#
+    aout = open('a.out', 'w')
+    for anya in horsedict:
+       aout.write(anya+"\n")
+ 
     # td in list []
     tddict = []
     tds = table.find_all("td", {"class": "htable_eng_text"})
+    hjson = hjson + ",{RACE: [{"
     for td in tds: 
         td = td.text.strip().encode("utf-8")
         if td == "(No Running Records in this season)":
@@ -50,17 +59,23 @@ def gethor(n,l):
         elif td == "":
            pass
         else:
-           file.write(td+"--end\n")
            tddict.append(td)
 
-    print(len(tddict))
-    for i in range(len(tddict)):
-	if i>18:
-           j = i%19
+
+    #testing#
+    bout = open('b.out', 'a')
+    bout.write(n+"\n")
+    for anyb in tddict:
+       bout.write(anyb+"\n")
+
+    for i in xrange(1,len(tddict),17):
+	if i>17:
+           j = i%18
         else:
            j = i
-        print(horsedict[j]+":"+tddict[i])
+        hjson = hjson + "'" + horsedict[j] + "':" + "'" +  tddict[i] + "' ,"
 
+    print(hjson)
+    #file.write(hjson)
 
 readhor()
-
